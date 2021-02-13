5ab7299b2ea690032632fb4a6a6241b6fa1c2490
brenobccc <brenobritobc@gmail.com>
2021-02-13 07:38:02 -0300

    exercicio_ POO

---
+++ exer2/Exercicio2.java
@@ -0,0 +1,15 @@
+/*2. Imprima a soma de 1 até 1000.*/
+
+class Exercicio2{
+	public static void main(String[] args) {
+		int soma=0;
+		for(int cont = 1; cont<=1000 ; cont++){
+			soma+=cont;
+		}
+
+
+		System.out.println("Soma: "+soma);
+			
+		
+	}
+}
---
+++ exer6/Exercicio6.java
@@ -0,0 +1,17 @@
+/*6. (opcional) Imprima os primeiros números da série de Fibonacci até passar de 100. A série de
+Fibonacci é a seguinte: 0, 1, 1, 2, 3, 5, 8, 13, 21, etc... Para calculá-la, o primeiro elemento vale 0, o
+segundo vale 1, daí por diante, o n-ésimo elemento vale o (n-1)-ésimo elemento somado ao (n-2)-
+ésimo elemento (ex: 8 = 5 + 3).*/
+
+class Exercicio6{
+	public static void main(String[] args) {
+		int n=0,m=1,o;
+
+		while(n<=100){
+			o=m;
+			System.out.println(n);
+			m+=n;
+			n=o;
+		}
+	}
+}
