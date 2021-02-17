91d02b17dad0232c3a0f87dac8eebafac72d28c8
brenobccc <brenobritobc@gmail.com>
2021-02-16 21:41:34 -0300

    atualização_questoesPOO

--- exer2/Exercicio2.java
+++ exer2/Exercicio2.java
@@ -1,6 +1,6 @@
 /*2. Imprima a soma de 1 até 1000.*/
 
-class Exercicio2{
+public class Exercicio2{
 	public static void main(String[] args) {
 		int soma=0;
 		for(int cont = 1; cont<=1000 ; cont++){
---
+++ exer4/Exercicio4.java
@@ -0,0 +1,21 @@
+/*
+O fatorial de um número n é n * (n-1) * (n-2) * ... * 1. Lembre-se de utilizar os parênteses.
+O fatorial de 0 é 1
+O fatorial de 1 é (0!) * 1 = 1
+O fatorial de 2 é (1!) * 2 = 2
+O fatorial de 3 é (2!) * 3 = 6
+O fatorial de 4 é (3!) * 4 = 24
+
+*/
+
+public class Exercicio4{
+	public static void main(String[] args) {
+		int fatorial=1;
+
+		for (int i=1;i<=10 ; i++ ){
+			fatorial*=i;
+			System.out.printf("%d ",fatorial);
+		}
+		
+	}
+}
